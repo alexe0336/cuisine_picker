6e45fd30029c6203c9c10a730c63434381281c47
Alex <alexe0336@gmail.com>
2024-06-12 01:33:55 -0400

    feat: Add cuisine picker component and configuration files
    The commit adds the following changes:
    - Added `babel.config.js` file
    - Added `main.js` file
    - Added `vue.config.js` file
    - Added `.gitignore` file
    - Added `jsconfig.json` file
    - Added `README.md` file
    - Added `App.vue` file
    - Added `index.html` file
    - Added `package.json` file
    - Added `CuisinePicker.vue` component
    
    These changes introduce the initial setup and configuration files for the cuisine picker project, along with the main application component.

---
+++ cuisine-picker/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <CuisinePicker />
+  </div>
+</template>
+
+<script>
+import CuisinePicker from './components/CuisinePicker.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CuisinePicker
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
---
+++ cuisine-picker/src/components/CuisinePicker.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div v-if="allCuisines.length > 1">
+      <button @click="selectCuisine(0)">
+        {{ allCuisines[0] }}
+      </button>
+      <button @click="selectCuisine(1)">
+        {{ allCuisines[1] }}
+      </button>
+    </div>
+    <div v-else>
+      <h1>You picked {{ allCuisines[0] }}!</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      allCuisines: ['Italian', 'Mexican', 'Chinese', 'Indian', 'Japanese', 'French', 'Thai', 'Spanish', 'Greek', 'American'],
+    };
+  },
+  methods: {
+    selectCuisine(index) {
+      // Add the selected cuisine to the front of the array
+      const selected = this.allCuisines[index];
+      this.allCuisines.splice(0, 2);
+      this.allCuisines.unshift(selected);
+    }
+  }
+};
+</script>
+
+<style scoped>
+button {
+  font-size: 3em;
+  margin: 30px;
+  padding: 50px 50px;
+}
+</style>
